df38c31c2c12a7c6a1923429ec7767971df0590c
Bhavika-Giyanani <bhavikavgiyanani@gmail.com>
2025-02-05 12:38:26 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server Activated at ${PORT}`);
+});
